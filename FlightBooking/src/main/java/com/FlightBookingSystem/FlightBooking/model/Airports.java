c144e0fc23810eb2be3303569572f6166bc3a48d
Gaurav Kumar Mahato <gauravmahato178.gkm@gmail.com>
2021-10-04 23:00:30 +0530

    updated models airports and user

---
+++ FlightBooking/src/main/java/com/FlightBookingSystem/FlightBooking/model/Airports.java
@@ -0,0 +1,32 @@
+package com.FlightBookingSystem.FlightBooking.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "airport_record")
+public class Airports {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String airportCode;
+    @Size(min = 2, max = 20)
+    @NotNull(message = "Pls Enter Airport Name!")
+    private String airportName;
+    @Size(min = 2, max = 20)
+
+    @NotNull(message = "Pls Enter Airport Location!")
+    private String airportLocation;
+
+
+    public Airports(String airportName, String airportLocation, String airportCode) {
+        this.airportName = airportName;
+        this.airportLocation = airportLocation;
+        this.airportCode = airportCode;
+    }
+}
---
+++ FlightBooking/src/main/java/com/FlightBookingSystem/FlightBooking/model/User.java
@@ -0,0 +1,44 @@
+package com.FlightBookingSystem.FlightBooking.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "user_record")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Size(min = 2, max = 20)
+    @NotNull(message = "Pls Enter User Name!")
+    private String username;
+    @Size(min = 2, max = 20)
+
+    @NotNull(message = "Pls Enter Password!")
+    private String password;
+
+    @NotNull(message = "Pls Enter phoneNo")
+    private Integer phoneNo;
+
+    @NotNull(message = "Pls Enter Email")
+    private String email;
+
+    @NotNull(message = "Pls Enter User Type")
+    private String userType;
+
+
+    public User(String username, String password,Integer phoneNo, String email, String userType,Integer id) {
+        this.username = username;
+        this.password = password;
+        this.phoneNo = phoneNo;
+        this.email = email;
+        this.userType = userType;
+        this.id = id;
+    }
+}
