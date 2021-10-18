ad43edc65f2210565064d8192a7493588c576208
Gaurav Kumar Mahato <gauravmahato178.gkm@gmail.com>
2021-10-18 12:13:33 +0530

    updated controller

---
+++ FlightBooking/src/main/java/com/FlightBookingSystem/FlightBooking/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.FlightBookingSystem.FlightBooking.controller;
+
+import com.FlightBookingSystem.FlightBooking.exception.RecordAlreadyExistException;
+import com.FlightBookingSystem.FlightBooking.exception.RecordNotFoundException;
+import com.FlightBookingSystem.FlightBooking.model.Users;
+import com.FlightBookingSystem.FlightBooking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+
+@ComponentScan(basePackages = "com")
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+	UserService userService;
+
+	@PostMapping("/createUser")
+	@ExceptionHandler(RecordAlreadyExistException.class)
+	public void addUser(@RequestBody Users newUser) {
+
+		userService.createUser(newUser);
+	}
+
+	@GetMapping("/readAllUsers")
+	public Iterable<Users> readAllUsers() {
+
+		return userService.displayAllUser();
+	}
+
+	@PutMapping("/updateUser")
+	@ExceptionHandler(RecordNotFoundException.class)
+	public void updateUser(@RequestBody Users updateUser) {
+
+		userService.updateUser(updateUser);
+	}
+
+	@GetMapping("/searchUser/{id}")
+	@ExceptionHandler(RecordNotFoundException.class)
+	public ResponseEntity<?> searchUserByID(@PathVariable("id") BigInteger userId) {
+
+		return userService.findUserById(userId);
+	}
+
+	@DeleteMapping("/deleteUser/{id}")
+	@ExceptionHandler(RecordNotFoundException.class)
+	public void deleteBookingByID(@PathVariable("id") BigInteger userId) {
+
+		userService.deleteUser(userId);
+	}
+}
