bd242e7fef135df83c3c9c49905a47cefabb0ba5
Gaurav Kumar Mahato <gauravmahato178.gkm@gmail.com>
2021-10-18 10:59:50 +0530

    updated models flight and booking

--- FlightBooking/src/main/java/com/FlightBookingSystem/FlightBooking/model/Booking.java
+++ FlightBooking/src/main/java/com/FlightBookingSystem/FlightBooking/model/Booking.java
@@ -1,4 +1,26 @@
 package com.FlightBookingSystem.FlightBooking.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "booking_record")
 public class Booking {
-}
+        @Id
+        @GeneratedValue(strategy= GenerationType.AUTO)
+        private BigInteger bookingId;
+        private String bookingDate;
+        private int noOfPassengers;
+
+        public Booking(BigInteger bookingId,String bookingDate,Integer noOfPassengers) {
+            this.bookingId=bookingId;
+            this.bookingDate=bookingDate;
+            this.noOfPassengers=noOfPassengers;
+        }
+
+    }
--- FlightBooking/src/main/java/com/FlightBookingSystem/FlightBooking/model/Users.java
+++ FlightBooking/src/main/java/com/FlightBookingSystem/FlightBooking/model/Users.java
@@ -11,34 +11,34 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 @Entity
 @Table(name = "users_record")
-public class User {
+public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Integer userId;
     @Size(min = 2, max = 20)
     @NotNull(message = "Pls Enter User Name!")
-    private String username;
+    private String userName;
     @Size(min = 2, max = 20)
 
     @NotNull(message = "Pls Enter Password!")
-    private String password;
+    private String userPassword;
 
     @NotNull(message = "Pls Enter phoneNo")
-    private Integer phoneNo;
+    private Integer userPhone;
 
     @NotNull(message = "Pls Enter Email")
-    private String email;
+    private String userEmail;
 
     @NotNull(message = "Pls Enter User Type")
     private String userType;
 
 
-    public User(String username, String password,Integer phoneNo, String email, String userType,Integer id) {
-        this.username = username;
-        this.password = password;
-        this.phoneNo = phoneNo;
-        this.email = email;
+    public Users(String userName, String userPassword, Integer userPhone, String userEmail, String userType, Integer userId) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.userPhone = userPhone;
+        this.userEmail = userEmail;
         this.userType = userType;
-        this.id = id;
+        this.userId = userId;
     }
 }
